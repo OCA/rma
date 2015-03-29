04ed24335b68f2d5636c92c840aca672495dc24c
Yanina Aular <yanina.aular@vauxoo.com>
2015-03-29 04:15:37 -0430

    [IMP] method to calculate stock quantity of product in rma is migrated to odoo 8.0

--- crm_rma_stock_location/product.py
+++ crm_rma_stock_location/product.py
@@ -21,66 +21,74 @@
 
 from openerp.osv import osv, fields
 import openerp.addons.decimal_precision as dp
+from openerp.tools.float_utils import float_round
 
 
-class ProductTemplate(osv.Model):
-    _inherit = 'product.template'
+class ProductProduct(osv.Model):
+    _inherit = 'product.product'
 
     def _rma_product_available(self, cr, uid, ids, field_names=None, arg=False,
                                context=None):
         """ Finds the incoming and outgoing quantity of product for the RMA
         locations.
         """
-        if field_names is None:
-            field_names = []
-        if context is None:
-            context = {}
-        warehouse_obj = self.pool['stock.warehouse']
         res = {}
-        for id in ids:
-            res[id] = {}.fromkeys(field_names, 0.0)
+        context = context or {}
 
-        for field in field_names:
-            ctx = context.copy()
+        warehouse_id = context.get('warehouse_id')
+        warehouse_obj = self.pool.get('stock.warehouse')
+        # no dependency on 'sale', the same oddness is done in
+        # 'stock' so I kept it here
+        ctx = context.copy()
 
-            warehouse_id = ctx.get('warehouse_id')
-            # no dependency on 'sale', the same oddness is done in
-            # 'stock' so I kept it here
+        if warehouse_id:
+            rma_id = warehouse_obj.read(cr, uid,
+                                        warehouse_id,
+                                        ['lot_rma_id'],
+                                        context=ctx)['lot_rma_id'][0]
+            if rma_id:
+                ctx['location'] = rma_id
+        else:
+            location_ids = set()
+            wids = warehouse_obj.search(cr, uid, [], context=context)
+            if not wids:
+                return res
+            for wh in warehouse_obj.browse(cr, uid, wids, context=context):
+                if wh.lot_rma_id:
+                    location_ids.add(wh.lot_rma_id.id)
+            if not location_ids:
+                return res
+            ctx['location'] = list(location_ids)
 
-            if warehouse_id:
-                rma_id = warehouse_obj.read(cr, uid,
-                                            warehouse_id,
-                                            ['lot_rma_id'],
-                                            context=ctx)['lot_rma_id'][0]
-                if rma_id:
-                    ctx['location'] = rma_id
-            else:
-                location_ids = set()
-                wids = warehouse_obj.search(cr, uid, [], context=context)
-                if not wids:
-                    return res
-                for wh in warehouse_obj.browse(cr, uid, wids, context=context):
-                    if wh.lot_rma_id:
-                        location_ids.add(wh.lot_rma_id.id)
-                if not location_ids:
-                    return res
-                ctx['location'] = list(location_ids)
+        field_names = field_names or []
 
-            ctx['compute_child'] = True
-            compute = {
-                'rma_qty_available': {
-                    'states': ('done', ),
-                    'what': ('in', 'out')
-                },
-                'rma_virtual_available': {
-                    'states': ('confirmed', 'waiting', 'assigned', 'done'),
-                    'what': ('in', 'out')
-                }
+        domain_products = [('product_id', 'in', ids)]
+        domain_quant, domain_move_in, domain_move_out = self._get_domain_locations(cr, uid, ids, context=ctx)
+        domain_move_in += self._get_domain_dates(cr, uid, ids, context=ctx) + [('state', 'not in', ('done', 'cancel', 'draft'))] + domain_products
+        domain_move_out += self._get_domain_dates(cr, uid, ids, context=ctx) + [('state', 'not in', ('done', 'cancel', 'draft'))] + domain_products
+        domain_quant += domain_products
+        if context.get('lot_rma_id'):
+            if context.get('lot_rma_id'):
+                domain_quant.append(('lot_rma_id', '=', context['lot_rma_id']))
+            moves_in = []
+            moves_out = []
+        else:
+            moves_in = self.pool.get('stock.move').read_group(cr, uid, domain_move_in, ['product_id', 'product_qty'], ['product_id'], context=ctx)
+            moves_out = self.pool.get('stock.move').read_group(cr, uid, domain_move_out, ['product_id', 'product_qty'], ['product_id'], context=ctx)
+        quants = self.pool.get('stock.quant').read_group(cr, uid, domain_quant, ['product_id', 'qty'], ['product_id'], context=ctx)
+        quants = dict(map(lambda x: (x['product_id'][0], x['qty']), quants))
+
+        moves_in = dict(map(lambda x: (x['product_id'][0], x['product_qty']), moves_in))
+        moves_out = dict(map(lambda x: (x['product_id'][0], x['product_qty']), moves_out))
+        res = {}
+        for product in self.browse(cr, uid, ids, context=context):
+            id = product.id
+            rma_qty_available = float_round(quants.get(id, 0.0), precision_rounding=product.uom_id.rounding)
+            rma_virtual_available = float_round(quants.get(id, 0.0) + moves_in.get(id, 0.0) - moves_out.get(id, 0.0), precision_rounding=product.uom_id.rounding)
+            res[id] = {
+                'rma_qty_available' : rma_qty_available,
+                'rma_virtual_available' : rma_virtual_available,
             }
-            ctx.update(compute[field])
-            stock = self.get_product_available(cr, uid, ids, context=ctx)
-            for id in ids:
-                res[id][field] = stock.get(id, 0.0)
         return res
 
     _columns = {
