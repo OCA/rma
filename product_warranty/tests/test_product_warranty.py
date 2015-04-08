38b46d7d1a50703e51c1346448056e86311925f1
Yanina Aular <yanina.aular@vauxoo.com>
2015-04-08 01:18:45 -0430

    [ADD] test in default=True in intruction to the create a record of product.supplierinfo. Capitalize menu name

---
+++ product_warranty/tests/test_product_warranty.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Yanina Aular
+#    Copyright 2015 Vauxoo
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.tests.common import TransactionCase
+
+
+class TestProductWarranty(TransactionCase):
+
+    def setUp(self):
+        super(TestProductWarranty, self).setUp()
+        self.instruction_model = self.env['return.instruction']
+        self.supplierinfo = self.env['product.supplierinfo']
+
+    def create_product_supplierinfo(self):
+        """
+        Create a record of product.supplier for next tests
+        """
+        cr, uid = self.cr, self.uid
+
+        product_tmpl_id = self.registry("ir.model.data").\
+            get_object_reference(cr, uid, "product",
+                                 "product_product_3")[1]
+
+        partner_id = self.registry("ir.model.data").\
+            get_object_reference(cr, uid, "base",
+                                 "res_partner_4")[1]
+
+        supplierinfo_data = dict(name=partner_id,
+                                 product_name='Test SupplierInfo for'
+                                 ' display Default Instruction',
+                                 min_qty=4,
+                                 delay=5,
+                                 warranty_return_partner='company',
+                                 product_tmpl_id=product_tmpl_id,)
+        self.supplierinfo_brw = \
+            self.supplierinfo.create(supplierinfo_data)
+
+    def test_default_instruction(self):
+        """
+        Test for return.instruction record with
+        default field in True. If is assigned
+        correctly when one record of
+        product.supplierinfo is created
+        """
+        cr, uid = self.cr, self.uid
+
+        self.create_product_supplierinfo()
+
+        return_instructions_id = self.registry("ir.model.data").\
+            get_object_reference(cr, uid, "product_warranty",
+                                 "return_instruction_1")[1]
+
+        self.assertEquals(self.supplierinfo_brw.return_instructions.id,
+                          return_instructions_id)
