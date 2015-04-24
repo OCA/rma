7ee8f841843494ff9cdbf77b798939fc2a7019f5
Yanina Aular <yanina.aular@vauxoo.com>
2015-04-24 16:23:35 -0430

    [FIX] account_invoice deleted from __init__ because was deleted

--- crm_rma_prodlot_invoice/model/__init__.py
+++ crm_rma_prodlot_invoice/model/__init__.py
@@ -23,5 +23,4 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ###############################################################################
 
-from . import account_invoice
 from . import sale
