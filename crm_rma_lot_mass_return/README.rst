d6611f6b305d412f3bc4d29852f8a469fa5fac77
Osval Reyes <osval@vauxoo.com>
2015-08-30 23:02:02 -0430

    [REF] crm_rma_lot_mass_return: apply OCA guidelines

---
+++ crm_rma_lot_mass_return/README.rst
@@ -0,0 +1,9 @@
+RMA Claim Mass Return by Lot
+============================
+
+This module adds possibility to return a whole lot of product from a Claim
+and create a incoming shipment for them.
+
+
+WARNING: This module is currently not yet completely debugged and is waiting
+his author to be.
--- crm_rma_lot_mass_return/wizards/__init__.py
+++ crm_rma_lot_mass_return/wizards/__init__.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
+#    Copyright 2015 Vauxoo
 #    Copyright 2013 Camptocamp
 #    Copyright 2009-2013 Akretion,
 #    Author: Emmanuel Samyn, Raphaël Valyi, Sébastien Beau,
-#            Joel Grand-Guillaume
+#            Joel Grand-Guillaume,
+#            Yanina Aular, Osval Reyes
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -20,4 +22,5 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+
 from . import returned_lines_from_serial
