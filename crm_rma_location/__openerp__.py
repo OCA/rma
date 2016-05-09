2f5716852cd4dd80006fae7ac6ff27fa088d720c
Cyril Gaudin <cyril.gaudin@camptocamp.com>
2016-05-09 11:07:56 +0200

    crm_rma_location: v9 migration

--- crm_rma_location/__openerp__.py
+++ crm_rma_location/__openerp__.py
@@ -1,27 +1,10 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright 2015 Vauxoo
-#    Author: Yanina Aular
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © Guewen Yanina Aular, Vauxoo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'RMA Location',
-    'version': '8.0.1.0.0',
+    'version': '9.0.1.0.0',
     'author': "Vauxoo, Odoo Community Association (OCA)",
     'license': 'AGPL-3',
     'website': 'http://www.camptocamp.com,http://www.vauxoo.com',
