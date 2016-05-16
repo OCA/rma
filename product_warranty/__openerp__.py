17b62369b5cadbbceab594f807dc84e2d0e180b0
Yanina Aular (Vauxoo) <yanina.aular@vauxoo.com>
2016-05-16 18:43:47 -0400

    [FIX] Deprecated key "active" in manifest file

--- product_warranty/__openerp__.py
+++ product_warranty/__openerp__.py
@@ -1,24 +1,8 @@
 # -*- coding: utf-8 -*-
-#########################################################################
-#                                                                       #
-#                                                                       #
-#########################################################################
-#                                                                       #
-# Copyright (C) 2009-2011  Akretion, Emmanuel Samyn                     #
-#                                                                       #
-# This program is free software: you can redistribute it and/or modify  #
-# it under the terms of the GNU General Public License as published by  #
-# the Free Software Foundation, either version 3 of the License, or     #
-# (at your option) any later version.                                   #
-#                                                                       #
-# This program is distributed in the hope that it will be useful,       #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
-# GNU General Public License for more details.                          #
-#                                                                       #
-# You should have received a copy of the GNU General Public License     #
-# along with this program.  If not, see <http://www.gnu.org/licenses/>. #
-#########################################################################
+# © 2016 Yanina Aular (Vauxoo)
+# © 2016 Cyril Gaudin (Camptocamp)
+# © 2009-2011  Akretion, Emmanuel Samyn
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Product warranty',
@@ -39,6 +23,5 @@
     ],
     'test': [],
     'installable': True,
-    'active': False,
     'images': ['images/product_warranty.png'],
 }
