cb8ae2d63e1d91279175b9bfb7d4d7f63c8c42c2
Techspawn Solutions <siddharth@techspawn.com>
2017-01-11 16:12:08 +0530

    changes in the crm_rma module and made it 10.0 compatible

--- crm_claim_rma/models/stock_picking.py
+++ crm_claim_rma/models/stock_picking.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
+# © 2017 Techspawn Solutions
 # © 2015 Eezee-It, MONK Software, Vauxoo
 # © 2013 Camptocamp
 # © 2009-2013 Akretion,
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class StockPicking(models.Model):
