a2bebfc8680036451323bae08ff7ff34eec10f90
bima <bimajatiwijaya@gmail.com>
2017-05-29 20:43:50 +0700

    migration to v10.0

---
+++ rma/models/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from . import account_invoice
+from . import account_invoice_line
+from . import claim_line
+from . import crm_claim
+from . import crm_claim_type
+from . import invoice_no_date
+from . import product_no_supplier
+from . import procurement_group
+from . import stock_move
+from . import stock_picking
+from . import substate_substate
