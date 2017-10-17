6496d634c21f0b8814a12441ab42622dc50a6ad0
Bhavesh Odedra <bodedra@ursainfosystems.com>
2017-10-17 17:10:00 +0530

    [IMP] manifest file

--- crm_claim_rma_code/__manifest__.py
+++ crm_claim_rma_code/__manifest__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 # © 2015 Vauxoo
-# © 2017 Ursa Information Systems
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
@@ -8,7 +7,7 @@
     'version': '10.0.1.0.0',
     'category': 'Generic Modules/CRM & SRM',
     'author': 'Vauxoo,Odoo Community Association (OCA)',
-    'website': 'http://www.vauxoo.com/, http://ursainfosystems.com/',
+    'website': 'https://github.com/OCA/rma/tree/10.0/crm_claim_rma_code',
     'license': 'AGPL-3',
     'depends': [
         'crm_claim_type',
