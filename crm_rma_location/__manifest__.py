55a732cbf5cbc5fc2c6dab429170cbc3e5eee310
Pragya Chhabra <pragya.chhabra@linserv.se>
2018-02-21 13:08:10 +0100

    [MIG] crm_rma_location: Migration to 11.0

--- crm_rma_location/__manifest__.py
+++ crm_rma_location/__manifest__.py
@@ -1,17 +1,15 @@
-# -*- coding: utf-8 -*-
 # © Guewen Yanina Aular, Vauxoo
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'RMA Location',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'author': "Vauxoo, Odoo Community Association (OCA)",
     'license': 'AGPL-3',
     'website': 'http://www.camptocamp.com,http://www.vauxoo.com',
     'category': 'Generic Modules/CRM & SRM',
     'depends': [
         'stock',
-        'procurement',
     ],
     'data': [
         'views/stock_warehouse.xml',
