ce805c6773d91f3cf124cb4b512a9d89c3a4235d
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2019-04-23 15:05:04 +0530

    [WIP][MIG][12.0] stock_production_lot_warranty

--- stock_production_lot_warranty/models/stock_production_lot.py
+++ stock_production_lot_warranty/models/stock_production_lot.py
@@ -1,9 +1,10 @@
 # Copyright (C) 2018 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models, api
-from dateutil.relativedelta import relativedelta
 from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+from odoo import api, fields, models
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT
 
 
 class StockProductionLot(models.Model):
@@ -13,24 +14,29 @@ class StockProductionLot(models.Model):
 
     @api.onchange('product_id')
     def _onchange_product_id(self):
-        if self.product_id:
-            if (self.product_id.product_tmpl_id.warranty_type and
-                    self.product_id.product_tmpl_id.warranty):
-                today_date = datetime.now()
-                if self.product_id.product_tmpl_id.warranty_type == 'day':
-                    time = (today_date +
-                            timedelta(days=self.product_id.
-                                      product_tmpl_id.warranty))
-                elif self.product_id.product_tmpl_id.warranty_type == 'week':
-                    time = (today_date +
-                            timedelta(weeks=self.product_id.
-                                      product_tmpl_id.warranty))
-                elif self.product_id.product_tmpl_id.warranty_type == 'month':
-                    time = (today_date +
-                            relativedelta(months=+self.product_id.
-                                          product_tmpl_id.warranty))
-                elif self.product_id.product_tmpl_id.warranty_type == 'year':
-                    time = (today_date +
-                            relativedelta(years=+self.product_id.
-                                          product_tmpl_id.warranty))
-                self.warranty_exp_date = time
+        self.warranty_exp_date = False
+        if self.product_id and \
+                self.product_id.product_tmpl_id.warranty_type and \
+                self.product_id.product_tmpl_id.warranty:
+            warranty_type = self.product_id.product_tmpl_id.warranty_type
+            if warranty_type == 'day':
+                time = (datetime.now() +
+                        timedelta(days=self.product_id.
+                                  product_tmpl_id.warranty)).strftime(
+                    DEFAULT_SERVER_DATE_FORMAT)
+            elif warranty_type == 'week':
+                time = (datetime.now() +
+                        timedelta(weeks=self.product_id.
+                                  product_tmpl_id.warranty)).strftime(
+                    DEFAULT_SERVER_DATE_FORMAT)
+            elif warranty_type == 'month':
+                time = (datetime.now() +
+                        relativedelta(months=+self.product_id.
+                                      product_tmpl_id.warranty)).strftime(
+                    DEFAULT_SERVER_DATE_FORMAT)
+            elif warranty_type == 'year':
+                time = (datetime.now() +
+                        relativedelta(years=+self.product_id.
+                                      product_tmpl_id.warranty)).strftime(
+                    DEFAULT_SERVER_DATE_FORMAT)
+            self.warranty_exp_date = time
