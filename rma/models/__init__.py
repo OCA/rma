a826a3d1aaf9833a5cc86543bf6c96360b83b61f
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2020-05-13 00:27:46 -0400

    [ADD] rma: new module
    [UPD] Update rma.pot
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: rma-12.0/rma-12.0-rma
    Translate-URL: https://translation.odoo-community.org/projects/rma-12-0/rma-12-0-rma/

---
+++ rma/models/__init__.py
@@ -0,0 +1,12 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from . import account_invoice
+from . import rma
+from . import rma_operation
+from . import rma_team
+from . import res_company
+from . import res_partner
+from . import res_users
+from . import stock_move
+from . import stock_picking
+from . import stock_warehouse
