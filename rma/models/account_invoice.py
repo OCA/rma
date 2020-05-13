36fa31bbd5dddf44708466bd90bd491882ff86c3
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2020-05-13 00:27:46 -0400

    [ADD] rma: new module
    [UPD] Update rma.pot
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: rma-12.0/rma-12.0-rma
    Translate-URL: https://translation.odoo-community.org/projects/rma-12-0/rma-12-0-rma/

---
+++ rma/models/account_invoice.py
@@ -0,0 +1,35 @@
+# Copyright 2020 Tecnativa - Ernesto Tejeda
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import _, fields, models
+from odoo.exceptions import ValidationError
+from odoo.tools import float_compare
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    def action_invoice_open(self):
+        """ Avoids to validate a refund with less quantity of product than
+        quantity in the linked RMA.
+        """
+        precision = self.env['decimal.precision'].precision_get(
+            'Product Unit of Measure')
+        if self.mapped('invoice_line_ids').filtered(
+                lambda r: (r.rma_id and float_compare(
+                    r.quantity, r.rma_id.product_uom_qty, precision) < 0)):
+            raise ValidationError(
+                _("There is at least one invoice lines whose quantity is "
+                  "less than the quantity specified in its linked RMA."))
+        res = super().action_invoice_open()
+        self.mapped('invoice_line_ids.rma_id').write({'state': 'refunded'})
+        return res
+
+
+class AccountInvoiceLine(models.Model):
+    _inherit = 'account.invoice.line'
+
+    rma_id = fields.Many2one(
+        comodel_name='rma',
+        string='RMA',
+    )
---
+++ rma/wizard/__init__.py
@@ -0,0 +1,5 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from . import rma_delivery
+from . import rma_split
+from . import stock_picking_return
