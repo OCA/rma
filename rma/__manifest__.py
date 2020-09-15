82d03445b4db457bae3b2eca74cfaa5a714093ef
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2020-09-15 16:04:43 -0400

    [IMP] rma: set rma to received on invoice delete + incoming picking type use existing lot by default.
    [IMP] rma: set rma to received on invoice delete.
    
    Remove 'waiting_refund' rma state.
    RMAs go from received to refunded.
    When the linked refund is deleted the rma is set to received.
    
    [UPD] Update rma.pot
    
    rma 12.0.1.4.2
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: rma-12.0/rma-12.0-rma
    Translate-URL: https://translation.odoo-community.org/projects/rma-12-0/rma-12-0-rma/
    
    [IMP] rma: incoming picking type use existing lot by default.
    
    rma 12.0.1.4.3

--- rma/__manifest__.py
+++ rma/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Return Merchandise Authorization Management",
     "summary": "Return Merchandise Authorization (RMA)",
-    "version": "12.0.1.4.1",
+    "version": "12.0.1.4.3",
     "development_status": "Beta",
     "category": "RMA",
     "website": "https://github.com/OCA/rma",
