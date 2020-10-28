65066bb8fe30f2af94224d3d070488780ca0e576
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2020-10-28 15:32:43 -0400

    [IMP] rma_sale: black, isort, prettier

--- rma_sale/controllers/rma_portal.py
+++ rma_sale/controllers/rma_portal.py
@@ -5,9 +5,8 @@ from odoo.addons.rma.controllers.main import PortalRma
 
 
 class PortalRma(PortalRma):
-
     def _get_filter_domain(self, kw):
         res = super()._get_filter_domain(kw)
-        if 'sale_id' in kw:
-            res.append(('order_id', '=', int(kw['sale_id'])))
+        if "sale_id" in kw:
+            res.append(("order_id", "=", int(kw["sale_id"])))
         return res
