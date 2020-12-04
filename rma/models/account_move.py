379a6c0350b5c9bdaf13c768302617115a23fd96
Chafique <chafique.delli@akretion.com>
2020-12-04 15:49:35 +0100

    [MIG] rma: Migration to 14.0

--- rma/models/account_move.py
+++ rma/models/account_move.py
@@ -27,7 +27,7 @@ class AccountMove(models.Model):
             )
         )
 
-    def post(self):
+    def action_post(self):
         """Avoids to validate a refund with less quantity of product than
         quantity in the linked RMA.
         """
@@ -38,7 +38,7 @@ class AccountMove(models.Model):
                     "less than the quantity specified in its linked RMA."
                 )
             )
-        return super().post()
+        return super().action_post()
 
     def unlink(self):
         rma = self.mapped("invoice_line_ids.rma_id")
