a59b3446b7f0066d309449a6c78222b1b89a486d
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2021-01-20 18:29:39 -0600

    [ADD] repair_line_forecast_qty: new module to show qty in stock in repair.line

---
+++ repair_line_forecast_qty/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Jarsa
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+
+{
+    "name": "Repair Line Forecast Qty",
+    "version": "14.0.1.0.0",
+    "category": "Repair",
+    "summary": "Show stock available qty in repair lines.",
+    "author": "Jarsa, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/rma",
+    "license": "LGPL-3",
+    "depends": ["repair", "sale_management"],
+    "data": ["views/repair_order_view.xml"],
+    "installable": True,
+}
