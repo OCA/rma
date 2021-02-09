6d96824913aa8c0f7c231925450dd32da57b2b21
david <david.vidal@tecnativa.com>
2021-02-09 13:50:02 +0100

    [IMP] rma: auto send confirmation email

--- rma/__manifest__.py
+++ rma/__manifest__.py
@@ -29,6 +29,7 @@
         "views/rma_views.xml",
         "views/stock_picking_views.xml",
         "views/stock_warehouse_views.xml",
+        "views/res_config_settings_views.xml",
     ],
     "post_init_hook": "post_init_hook",
     "application": True,
