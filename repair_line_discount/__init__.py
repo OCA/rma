449fdd61ad8416088aa94298922bbfee59a592cb
jesus01x <jesus.alcala@jarsa.com.mx>
2021-03-09 12:58:15 -0600

    [ADD] added a new module repair_line_discount this module allow use _discount lines of models repair fee and repair line. This discount was added to invoice_line_ids.

---
+++ repair_line_discount/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2021, Jarsa Sistemas, S.A. de C.V.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from . import models
---
+++ repair_line_discount/models/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2021, Jarsa Sistemas, S.A. de C.V.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from . import repair_fee
+from . import repair_line
