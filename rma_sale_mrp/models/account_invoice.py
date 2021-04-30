391c96f0ee7ea6476830c408ff642256fc88ae13
david <david.vidal@tecnativa.com>
2021-04-30 18:29:34 +0200

    [IMP] rma_sale_mrp: black, isort, prettier

--- rma_sale_mrp/models/account_invoice.py
+++ rma_sale_mrp/models/account_invoice.py
@@ -10,10 +10,14 @@ class AccountInvoice(models.Model):
     def _check_rma_invoice_lines_qty(self):
         """For those with differences, check if the kit quantity is the same"""
         precision = self.env["decimal.precision"].precision_get(
-            "Product Unit of Measure")
+            "Product Unit of Measure"
+        )
         lines = super()._check_rma_invoice_lines_qty()
         if lines:
             return lines.sudo().filtered(
-                lambda r: (r.rma_id.phantom_bom_product and float_compare(
-                    r.quantity, r.rma_id.kit_qty, precision) < 0))
+                lambda r: (
+                    r.rma_id.phantom_bom_product
+                    and float_compare(r.quantity, r.rma_id.kit_qty, precision) < 0
+                )
+            )
         return lines
