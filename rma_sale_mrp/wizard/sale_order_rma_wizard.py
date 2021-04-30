bf521bdb6074dfad9c16465b3bdc24a5e07661e2
david <david.vidal@tecnativa.com>
2021-04-30 18:29:34 +0200

    [IMP] rma_sale_mrp: black, isort, prettier

--- rma_sale_mrp/wizard/sale_order_rma_wizard.py
+++ rma_sale_mrp/wizard/sale_order_rma_wizard.py
@@ -20,17 +20,18 @@ class SaleOrderRmaWizard(models.TransientModel):
         """Split component lines"""
         if "line_ids" in vals and vals.get("line_ids"):
             line_ids = [
-                (x[0], x[1], x[2]) for x in vals.get("line_ids")
+                (x[0], x[1], x[2])
+                for x in vals.get("line_ids")
                 if not x[2].get("phantom_bom_product")
             ]
             component_line_ids = [
-                (x[0], x[1], x[2]) for x in vals.get("line_ids")
+                (x[0], x[1], x[2])
+                for x in vals.get("line_ids")
                 if x[2].get("phantom_bom_product")
             ]
-            vals.update({
-                "line_ids": line_ids,
-                "component_line_ids": component_line_ids,
-            })
+            vals.update(
+                {"line_ids": line_ids, "component_line_ids": component_line_ids}
+            )
         return super().create(vals)
 
     def create_rma(self, from_portal=None):
@@ -52,24 +53,28 @@ class SaleOrderRmaWizard(models.TransientModel):
                     lambda x: x.product_id == product and x.quantity
                 )
                 if not product_kit_component_lines:
-                    raise ValidationError(_(
-                        "The kit corresponding to the product %s can't be "
-                        "put in the RMA. Either all or some of the components "
-                        "where already put in another RMA"
-                    ) % line.product_id.name)
+                    raise ValidationError(
+                        _(
+                            "The kit corresponding to the product %s can't be "
+                            "put in the RMA. Either all or some of the components "
+                            "where already put in another RMA"
+                        )
+                        % line.product_id.name
+                    )
                 qty_to_return = (
-                    product_kit_component_lines[0].per_kit_quantity
-                    * line.quantity
+                    product_kit_component_lines[0].per_kit_quantity * line.quantity
                 )
                 while qty_to_return > 0:
                     for kit_line in product_kit_component_lines:
                         kit_line.quantity = min(qty_to_return, kit_line.quantity)
                         kit_line.operation_id = line.operation_id
                         kit_line.kit_qty_done = (
-                            kit_line.quantity / kit_line.per_kit_quantity)
+                            kit_line.quantity / kit_line.per_kit_quantity
+                        )
                         qty_to_return -= kit_line.quantity
             kit_line_vals += [
-                (0, 0, x._convert_to_write(x._cache)) for x in kit_component_lines]
+                (0, 0, x._convert_to_write(x._cache)) for x in kit_component_lines
+            ]
         self.update({"line_ids": kit_line_vals})
         # We don't need the phantom lines anymore as we already have the
         # kit component ones.
@@ -80,16 +85,12 @@ class SaleOrderRmaWizard(models.TransientModel):
 class SaleOrderLineRmaWizard(models.TransientModel):
     _inherit = "sale.order.line.rma.wizard"
 
-    phantom_bom_product = fields.Many2one(
-        comodel_name="product.product",
-    )
+    phantom_bom_product = fields.Many2one(comodel_name="product.product",)
     kit_qty_done = fields.Float(
         readonly=True,
         help="Used to inform kit qty used in the rma. Will be useful to refund",
     )
-    per_kit_quantity = fields.Float(
-        readonly=True,
-    )
+    per_kit_quantity = fields.Float(readonly=True,)
     phantom_kit_line = fields.Boolean(readonly=True)
 
     @api.depends("picking_id")
@@ -97,32 +98,33 @@ class SaleOrderLineRmaWizard(models.TransientModel):
         """We need to process kit components separately so we can match them
         against their phantom product"""
         not_kit = self.filtered(
-            lambda x: not x.phantom_bom_product and
-            not x.product_id._is_phantom_bom())
+            lambda x: not x.phantom_bom_product and not x.product_id._is_phantom_bom()
+        )
         super(SaleOrderLineRmaWizard, not_kit)._compute_move_id()
-        for line in self.filtered(
-                lambda x: x.phantom_bom_product and x.picking_id):
+        for line in self.filtered(lambda x: x.phantom_bom_product and x.picking_id):
             line.move_id = line.picking_id.move_lines.filtered(
                 lambda ml: (
                     ml.product_id == line.product_id
                     and ml.sale_line_id == line.sale_line_id
                     and ml.sale_line_id.product_id == line.phantom_bom_product
-                    and ml.sale_line_id.order_id == line.order_id))
+                    and ml.sale_line_id.order_id == line.order_id
+                )
+            )
 
     def _prepare_rma_values(self):
         """It will be used as a reference for the components"""
         res = super()._prepare_rma_values()
         if self.phantom_bom_product:
             unique_register = "{}-{}-{}".format(
-                self.wizard_id.id,
-                self.phantom_bom_product.id,
-                self.sale_line_id.id
+                self.wizard_id.id, self.phantom_bom_product.id, self.sale_line_id.id
             )
-            res.update({
-                "phantom_bom_product": self.phantom_bom_product.id,
-                "kit_qty": self.kit_qty_done,
-                "rma_kit_register": unique_register,
-            })
+            res.update(
+                {
+                    "phantom_bom_product": self.phantom_bom_product.id,
+                    "kit_qty": self.kit_qty_done,
+                    "rma_kit_register": unique_register,
+                }
+            )
         return res
 
 
