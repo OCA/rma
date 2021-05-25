9b4aa371d76d26ec17fdbfe84912338a119928f7
david <david.vidal@tecnativa.com>
2021-05-25 12:01:54 +0200

    [IMP] rma: public tags
    Now we can decide which tags are visible for the customer. This way, we
    can use them as pseudo-states
    
    TT29594

--- rma/models/rma_tag.py
+++ rma/models/rma_tag.py
@@ -19,6 +19,10 @@ class RmaTag(models.Model):
         translate=True,
         copy=False,
     )
+    is_public = fields.Boolean(
+        string="Public Tag",
+        help="The tag is visible in the portal view",
+    )
     color = fields.Integer(string="Color Index")
     rma_ids = fields.Many2many(comodel_name="rma")
 
