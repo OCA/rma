e93a840ccb9f91498d1a1ce3de3ce94c47122d87
david <david.vidal@tecnativa.com>
2021-07-14 08:23:31 +0200

    [IMP] rma: allow to archive operation types
    TT30985

--- rma/models/rma_operation.py
+++ rma/models/rma_operation.py
@@ -8,6 +8,7 @@ class RmaOperation(models.Model):
     _name = "rma.operation"
     _description = "RMA requested operation"
 
+    active = fields.Boolean(default=True)
     name = fields.Char(required=True, translate=True)
 
     _sql_constraints = [
