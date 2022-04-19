0dd93069b3c153fa04a367ef37b3544cebd8eeb6
Michael Tietz <mtietz@mt-software.de>
2022-04-19 16:52:29 +0200

    rma sale imporvements

--- rma_sale/models/rma.py
+++ rma_sale/models/rma.py
@@ -2,39 +2,40 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
+from odoo.osv import expression
+
+ORDER_STATE_DOMAIN = [
+    ('state', 'in', ['sale', 'done'])
+]
 
 
 class Rma(models.Model):
     _inherit = "rma"
+    
+    def _domain_product_id(self):
+        return [("type", "in", ["consu", "product"])]
 
     order_id = fields.Many2one(
-        comodel_name="sale.order",
-        string="Sale Order",
-        domain="["
-        "    ('partner_id', 'child_of', commercial_partner_id),"
-        "    ('state', 'in', ['sale', 'done']),"
-        "]",
+        "sale.order",
+        "Sale Order",
+        domain=ORDER_STATE_DOMAIN,
         readonly=True,
         states={"draft": [("readonly", False)]},
     )
     allowed_picking_ids = fields.Many2many(
-        comodel_name="stock.picking",
+        "stock.picking",
         compute="_compute_allowed_picking_ids",
     )
     picking_id = fields.Many2one(domain="[('id', 'in', allowed_picking_ids)]")
     allowed_move_ids = fields.Many2many(
-        comodel_name="sale.order.line",
+        "sale.order.line",
         compute="_compute_allowed_move_ids",
     )
     move_id = fields.Many2one(domain="[('id', 'in', allowed_move_ids)]")
     sale_line_id = fields.Many2one(
         related="move_id.sale_line_id",
     )
-    allowed_product_ids = fields.Many2many(
-        comodel_name="product.product",
-        compute="_compute_allowed_product_ids",
-    )
-    product_id = fields.Many2one(domain="[('id', 'in', allowed_product_ids)]")
+    product_id = fields.Many2one(domain=lambda self: self._domain_product_id())
 
     @api.depends("partner_id", "order_id")
     def _compute_allowed_picking_ids(self):
@@ -58,30 +59,78 @@ class Rma(models.Model):
             else:
                 rec.allowed_move_ids = self.picking_id.move_lines.ids
 
-    @api.depends("order_id")
-    def _compute_allowed_product_ids(self):
-        for rec in self:
-            if rec.order_id:
-                order_product = rec.order_id.order_line.mapped("product_id")
-                rec.allowed_product_ids = order_product.filtered(
-                    lambda r: r.type in ["consu", "product"]
-                ).ids
-            else:
-                rec.allowed_product_ids = (
-                    self.env["product.product"]
-                    .search([("type", "in", ["consu", "product"])])
-                    .ids
-                )
-
     @api.onchange("partner_id")
     def _onchange_partner_id(self):
         res = super()._onchange_partner_id()
-        self.order_id = False
+        
+        def add_order_domain(r, domain):
+            r = r or {}
+            _domain = r.setdefault('domain', {})
+            _domain['order_id'] = domain
+            return r
+        
+        PARTNER_DOMAIN = [(
+            'partner_id',
+            'child_of',
+            self.partner_id.commercial_partner_id.id
+        )]
+        if not self.partner_id or not self.order_id.filtered_domain(PARTNER_DOMAIN):
+            self.order_id = False
+            if not self.partner_id:
+                res = add_order_domain(
+                    res,
+                    ORDER_STATE_DOMAIN
+                )
+            else:
+                res = add_order_domain(
+                    res,
+                    expression.AND([
+                        ORDER_STATE_DOMAIN,
+                        PARTNER_DOMAIN,
+                    ])
+                )
         return res
 
     @api.onchange("order_id")
     def _onchange_order_id(self):
         self.product_id = self.picking_id = False
+        domain = self._domain_product_id()
+        if self.order_id:
+            domain += [
+                ('id', 'in', self.order_id.order_line.mapped('product_id').ids)
+            ]
+            self.partner_id = self.order_id.partner_id
+            self.partner_invoice_id = self.order_id.partner_invoice_id
+            self.partner_shipping_id = self.order_id.partner_shipping_id
+        return {"domain": {"product_id": domain}}
+    
+    @api.onchange("order_id", "product_id")
+    def _onchange_order_id_product_id(self):
+        if not self.order_id or not self.product_id:
+            self.picking_id = self.move_id = False
+            return
+
+        allowed_moves = self.allowed_picking_ids.filtered(
+            lambda p: self.product_id in p.move_lines.product_id
+        ).move_lines
+        
+        if not allowed_moves:
+            self.picking_id = self.move_id = False
+            return
+
+        last_move = allowed_moves.sorted(key=lambda m: m.picking_id.date_done, reverse=True)[0]
+        self.move_id = last_move
+        self.picking_id = last_move.picking_id
+
+    @api.onchange("picking_id")
+    def _onchange_picking_id(self):
+        if not self.picking_id:
+            self.move_id = False
+
+        if self.move_id not in self.picking_id.move_lines and self.product_id:
+            self.move_id = self.picking_id.move_lines.filtered(
+                lambda m: m.product_id == self.product_id
+            )
 
     def _prepare_refund(self, invoice_form, origin):
         """Inject salesman from sales order (if any)"""
