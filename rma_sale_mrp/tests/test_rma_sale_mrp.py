990e401afb75dbff7ef29af8129d0afc749ca5ee
Víctor Martínez <victor.martinez@tecnativa.com>
2022-10-10 15:10:33 +0200

    [MIG] rma_sale_mrp: Migration to 15.0
    TT37303

--- rma_sale_mrp/tests/test_rma_sale_mrp.py
+++ rma_sale_mrp/tests/test_rma_sale_mrp.py
@@ -1,25 +1,21 @@
 # Copyright 2020 Tecnativa - David Vidal
+# Copyright 2022 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo.exceptions import UserError, ValidationError
-from odoo.tests import Form, SavepointCase
+from odoo.tests import Form
+
+from odoo.addons.rma_sale.tests.test_rma_sale import TestRmaSaleBase
 
 
-class TestRmaSaleMrp(SavepointCase):
+class TestRmaSaleMrp(TestRmaSaleBase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.res_partner = cls.env["res.partner"]
-        cls.product_product = cls.env["product.product"]
-        cls.sale_order = cls.env["sale.order"]
         cls.product_kit = cls.product_product.create(
             {"name": "Product test 1", "type": "consu"}
         )
-        cls.product_kit_comp_1 = cls.product_product.create(
-            {"name": "Product Component 1", "type": "product"}
-        )
-        cls.product_kit_comp_2 = cls.product_product.create(
-            {"name": "Product Component 2", "type": "product"}
-        )
+        cls.product_kit_comp_1 = cls.product_1
+        cls.product_kit_comp_2 = cls.product_2
         cls.bom = cls.env["mrp.bom"].create(
             {
                 "product_id": cls.product_kit.id,
@@ -42,13 +38,8 @@ class TestRmaSaleMrp(SavepointCase):
         cls.product_2 = cls.product_product.create(
             {"name": "Product test 2", "type": "product"}
         )
-        cls.partner = cls.res_partner.create({"name": "Partner test"})
-        order_form = Form(cls.sale_order)
-        order_form.partner_id = cls.partner
-        with order_form.order_line.new() as line_form:
-            line_form.product_id = cls.product_kit
-            line_form.product_uom_qty = 5
-        cls.sale_order = order_form.save()
+
+        cls.sale_order = cls._create_sale_order(cls, [[cls.product_kit, 5]])
         cls.sale_order.action_confirm()
         # Maybe other modules create additional lines in the create
         # method in sale.order model, so let's find the correct line.
@@ -62,7 +53,7 @@ class TestRmaSaleMrp(SavepointCase):
             line.quantity_done = line.product_uom_qty - 7
         wiz_act = cls.order_out_picking.button_validate()
         wiz = Form(
-            cls.env[wiz_act["res_model"]].with_context(wiz_act["context"])
+            cls.env[wiz_act["res_model"]].with_context(**wiz_act["context"])
         ).save()
         wiz.process()
         cls.backorder = cls.sale_order.picking_ids - cls.order_out_picking
@@ -73,8 +64,7 @@ class TestRmaSaleMrp(SavepointCase):
     def test_create_rma_from_so(self):
         order = self.sale_order
         out_pickings = self.order_out_picking + self.backorder
-        wizard_id = order.action_create_rma()["res_id"]
-        wizard = self.env["sale.order.rma.wizard"].browse(wizard_id)
+        wizard = self._rma_sale_wizard(order)
         wizard.line_ids.quantity = 4
         res = wizard.create_and_open_rma()
         rmas = self.env["rma"].search(res["domain"])
@@ -149,3 +139,15 @@ class TestRmaSaleMrp(SavepointCase):
         wizard.line_ids.quantity = 1
         with self.assertRaises(ValidationError):
             wizard.create_and_open_rma()
+
+    def test_report_rma(self):
+        wizard = self._rma_sale_wizard(self.sale_order)
+        wizard.line_ids.quantity = 4
+        res = wizard.create_and_open_rma()
+        rmas = self.env["rma"].search(res["domain"])
+        for rma in rmas:
+            res = self.report_model._get_report_from_name(
+                "rma.report_rma"
+            )._render_qweb_text(rma.ids, False)
+            self.assertRegex(str(res[0]), self.product_kit_comp_1.name)
+            self.assertRegex(str(res[0]), self.product_kit_comp_2.name)
