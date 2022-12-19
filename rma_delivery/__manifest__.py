3a57b35f01b6187baee65e5aac09e1006078104c
nikolausweingartmair <nikolaus.weingartmair@grueneerde.com>
2022-12-19 16:12:59 +0100

    [MIG] rma_delivery: Migration to 15.0

--- rma_delivery/__manifest__.py
+++ rma_delivery/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Return Merchandise Authorization Management - Link with deliveries",
     "summary": "Allow to choose a default delivery carrier for returns",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "development_status": "Beta",
     "category": "RMA",
     "website": "https://github.com/OCA/rma",
