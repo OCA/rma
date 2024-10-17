3a71bc9ec459ca4eff10322721c00d5238f2c3f0
sbejaoui <souheil.bejaoui@acsone.eu>
2024-10-17 10:02:22 +0200

    [IMP] rma_lot: add onchange lot&product

--- rma_sale_lot/wizards/__init__.py
+++ rma_sale_lot/wizards/__init__.py
@@ -1,2 +1,1 @@
-from . import sale_order_rma_wizard
 from . import sale_order_line_rma_wizard
--- rma_sale_lot/wizards/sale_order_rma_wizard.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2024 ACSONE SA/NV
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
-from odoo import models
-
-
-class SaleOrderRmaWizard(models.TransientModel):
-
-    _inherit = "sale.order.rma.wizard"
