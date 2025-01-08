78daca252bac7b52f2f6540d5ce7e02ebc36e409
sbejaoui <souheil.bejaoui@acsone.eu>
2025-01-08 18:58:51 +0100

    [IMP] rma: improve rma report layout
    [UPD] Update rma_sale.pot
    
    [BOT] post-merge updates
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: rma-16.0/rma-16.0-rma_sale
    Translate-URL: https://translation.odoo-community.org/projects/rma-16-0/rma-16-0-rma_sale/

--- rma_sale/__manifest__.py
+++ rma_sale/__manifest__.py
@@ -6,7 +6,7 @@
 {
     "name": "Return Merchandise Authorization Management - Link with Sales",
     "summary": "Sale Order - Return Merchandise Authorization (RMA)",
-    "version": "16.0.3.0.0",
+    "version": "16.0.3.1.0",
     "development_status": "Production/Stable",
     "category": "RMA",
     "website": "https://github.com/OCA/rma",
