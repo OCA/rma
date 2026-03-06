db493f64a04f0efe9da64e1e92c6ecff79640ef9
Marie Lejeune <marie.lejeune@acsone.eu>
2026-03-06 09:19:28 +0100

    [FIX] rma_sale: compute allowed_qty in case of multi-step route

--- rma_sale/tests/test_rma_sale_allowed_qty.py
+++ rma_sale/tests/test_rma_sale_allowed_qty.py
@@ -52,6 +52,69 @@ class TestRmaSaleQuantityAllowed(TransactionCase):
         self.assertEqual(self.picking.state, "done")
         self.assertEqual(self.so.order_line.qty_delivered, qty)
 
+    def _create_rma_route_in_2_steps(self):
+        """
+        2 steps return route: Customer -> RMA Entrance -> RMA QC OK.
+        """
+        rma_entrance = self.env["stock.location"].create(
+            {
+                "name": "RMA: entrance before quality check",
+                "usage": "internal",
+            }
+        )
+        rma_reception = self.env["stock.picking.type"].create(
+            {
+                "name": "RMA Receptions",
+                "code": "incoming",
+                "sequence_code": "RMA-IN-REC",
+                "default_location_src_id": self.env.ref(
+                    "stock.stock_location_customers"
+                ).id,
+                "default_location_dest_id": rma_entrance.id,
+            }
+        )
+        rma_qc = self.env["stock.picking.type"].create(
+            {
+                "name": "RMA QC",
+                "code": "internal",
+                "sequence_code": "RMA-QC",
+                "default_location_src_id": rma_entrance.id,
+                "default_location_dest_id": self.env.ref(
+                    "stock.stock_location_stock"
+                ).id,
+            }
+        )
+        self.env["stock.route"].create(
+            {
+                "name": "RMA: In in 2 steps",
+                "warehouse_selectable": "True",
+                "warehouse_ids": [Command.link(self.warehouse.id)],
+                "rule_ids": [
+                    Command.create(
+                        {
+                            "name": "RMA Reception",
+                            "action": "pull",
+                            "picking_type_id": rma_reception.id,
+                            "procure_method": "make_to_stock",
+                            "location_src_id": rma_reception.default_location_src_id.id,
+                            "location_dest_id": rma_entrance.id,
+                        },
+                    ),
+                    Command.create(
+                        {
+                            "name": "RMA QC",
+                            "action": "push",
+                            "picking_type_id": rma_qc.id,
+                            "auto": "manual",
+                            "location_src_id": rma_entrance.id,
+                            "location_dest_id": rma_qc.default_location_dest_id.id,
+                        },
+                    ),
+                ],
+            }
+        )
+        self.warehouse.rma_loc_id = rma_entrance
+
     def test_1(self):
         """
         Test rma wizard:
@@ -109,3 +172,34 @@ class TestRmaSaleQuantityAllowed(TransactionCase):
         ):
             wizard.line_ids.quantity = 5
         wizard.line_ids.quantity = 1
+
+    def test_4(self):
+        """
+        Test rma wizard:
+
+            - deliver the so
+            - return 1 unit with a return route generating 2 moves
+            - open rma wizard
+        expected:
+            - qty proposed: 4
+            - allowed qty 4
+            - qty 0 if is_return_all = False
+        """
+        self._create_rma_route_in_2_steps()
+        self._deliver(5)
+        wizard = self._get_rma_wizard()
+        wizard.line_ids[0].quantity = 1
+        wizard.operation_id = self.env.ref("rma.rma_operation_refund").id
+        rma = wizard.create_rma()
+        rma.action_confirm()
+        rma.reception_move_id.picking_id.button_validate()
+        self.assertTrue(
+            rma.reception_move_id.move_dest_ids,
+            "Second step move for return should have been created",
+        )
+        rma.reception_move_id.move_dest_ids.picking_id.button_validate()
+        # Returned product was received, QC ok. Both steps performed
+        wizard = self._get_rma_wizard()
+        self.assertEqual(len(wizard.line_ids), 1)
+        self.assertEqual(wizard.line_ids.quantity, 4)
+        self.assertEqual(wizard.line_ids.allowed_quantity, 4)
