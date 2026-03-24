358a038a6d8d82c9534e91261274832465f8bae8
Alejandro Aladro <alejandroaladrogonzalez@gmail.com>
2026-03-24 10:08:32 +0100

    [FIX] rma_sale: Wrong method called in stock_picking_return

--- rma_sale/wizard/stock_picking_return.py
+++ rma_sale/wizard/stock_picking_return.py
@@ -17,13 +17,9 @@ class ReturnPicking(models.TransientModel):
             sale_order.partner_shipping_id,
         )
 
-    def _prepare_rma_values(self):
-        vals = super()._prepare_rma_values()
+    def _prepare_rma_vals(self):
+        vals = super()._prepare_rma_vals()
         sale_order = self.picking_id.sale_id
         if sale_order:
-            vals.update(
-                {
-                    "order_id": sale_order.id,
-                }
-            )
+            vals["order_id"] = sale_order.id
         return vals
