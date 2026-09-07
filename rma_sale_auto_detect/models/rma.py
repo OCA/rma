d1eed6f29ef1e9da1fd4638c0dbd83bd32ea9f2d
Denis Roussel <denis.roussel@acsone.eu>
2026-09-07 13:35:22 +0200

    [IMP] rma_sale_auto_detect: Keep the original state for the extra quantity

--- rma_sale_auto_detect/models/rma.py
+++ rma_sale_auto_detect/models/rma.py
@@ -116,6 +116,13 @@ class Rma(models.Model):
     def _sort_sale_lines_by_order_date(self, sale_lines):
         return sale_lines.sorted(lambda sol: (sol.order_id.date_order, sol.id))
 
+    def _prepare_new_rma_for_sale_link_values(self, qty):
+        self.ensure_one()
+        return {
+            "state": self.state,  # Bypass here the copy=False
+            "product_uom_qty": qty,
+        }
+
     def _link_rma_to_sale_line(self, sale_lines):
         """match between rmas and sale lines"""
         if not sale_lines:
@@ -147,7 +154,9 @@ class Rma(models.Model):
             elif rma_qty > remaining_qty:
                 # rma needs more than available on this sale line
                 # we copy RMA for the matched qty
-                matched_rma = rma.copy({"product_uom_qty": remaining_qty})
+                matched_rma = rma.copy(
+                    self._prepare_new_rma_for_sale_link_values(qty=remaining_qty)
+                )
                 # reduce qty on original RMA
                 rma.product_uom_qty = rma_qty - remaining_qty
                 # link the matched copy to the sale line
